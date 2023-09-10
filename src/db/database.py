35cc461df80623234fa37ba70c0416aaae06d7a1
Mercy <mercyclassic123@gmail.com>
2023-09-10 17:27:46 +0300

    fix path to db

--- src/db/database.py
+++ src/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
-DATABASE_URL = 'sqlite:///sqlite.db'
+DATABASE_URL = 'sqlite:///../sqlite.db'
 
 engine = create_engine(DATABASE_URL)
 session_maker = sessionmaker(engine, class_=Session, expire_on_commit=False)
